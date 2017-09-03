0e87288dd71acdb1159d53a183cc4fbe65633334
deruga04 <derekli.3628@gmail.com>
2017-09-02 22:48:31 -0400

    added mock bot

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key: "1rZHdJOdLBxEdT9zHvxjBOtRt",
-	consumer_secret: "H3sFEtzsGlhMVJGUIUEdyT1qLMZB2CpHCtN8E7TNnQLzDmpdrk",
-	access_token_key: "716791198132862976-bUplBhTjDNSQV8IlV7Ip9rf8ivnwDlD",
-	access_token_secret: "OXGqqA1xG9XVGc2jnTkzHBSIsyrbmzFCHEsEKGJLI5s5U"
-}
